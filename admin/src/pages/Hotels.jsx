22acb1e868ad97f23c505df758e46dc421e144f1
Oleksandr-web29 <Bohdan.web29@gmail.com>
2023-11-28 21:59:27 +0800

    Fix Firebase Integration

--- admin/src/pages/Hotels.jsx
+++ admin/src/pages/Hotels.jsx
@@ -1,24 +1,20 @@
-import Box  from "@mui/material/Box";
+import Box from "@mui/material/Box";
 import Navbar from "../components/Navbar";
 import Sidenav from "../components/sidenav";
 import HotelList from "./hotels/HotelList";
 
-
-
-export default function Hotels()  {
-  return (
-    <>
-    <Navbar />
-        <Box height={70} />
-      <Box sx={{ display: "flex", marginLeft: "500"}}>
-      <Sidenav />
-        <h1>Hotels</h1>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <HotelList/>
-      </Box>
-      </Box>
-    </>
-  );
-};
-
-
+export default function Hotels() {
+   return (
+      <>
+         <Navbar />
+         <Box height={70} />
+         <Box sx={{ display: "flex", marginLeft: "500" }}>
+            <Sidenav />
+            <Box component="main" sx={{ flexGrow: 1, px:3, py:1 }}>
+               <h1>Hotels</h1>
+               <HotelList />
+            </Box>
+         </Box>
+      </>
+   );
+}
--- admin/src/pages/hotels/HotelList.jsx
+++ admin/src/pages/hotels/HotelList.jsx
@@ -1,122 +1,143 @@
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-import { useState, useEffect } from 'react';
-import { getFirestore, collection, getDocs, doc } from 'firebase/firestore';
-import db  from '../../firebase-config-jsx';
+import { useState, useEffect } from "react";
+import {
+   getFirestore,
+   collection,
+   getDocs,
+   doc,
+} from "firebase/firestore/lite";
+import { db, auth } from "../../firebase-config";
+import { Link } from "react-router-dom";
 
-const rows = [
- 
-];
+const rows = [];
 
 export default function HotelList() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [rows, setRows] = useState([]);
-  
-  
-  
-  useEffect(() => {
-    getUsers();
-  }, []);
+   const [page, setPage] = useState(0);
+   const [rowsPerPage, setRowsPerPage] = useState(10);
+   const [rows, setRows] = useState([]);
 
-  const getUsers = async () => {
-    try{
-      const empCollectionRef = collection(db, "places");
-      const data = await getDocs(empCollectionRef);
-      setRows(data.docs.map((doc) => ({ ...doc.data(), id: doc.id 
-      })))
-      console.log('fetched hotels:', data)
-    } catch(error) {
-      console.error("Error fetching hotels:", error);
-    }
-  }
+   useEffect(() => {
+      getUsers();
+   }, []);
 
-  // const fetchHotels = async () => {
-  //   try {
-  //     const hotelsCollection = collection(db, "places");
-  //     const hotelsSnapshot = await getDocs(hotelsCollection);
-  //     const hotelsData = hotelsSnapshot.docs.map((doc) => ({
-  //       ...doc.data(),
-  //       id: doc.id
-  //     }));
-  //     console.error("fetched hotels:", hotelsData);
-  //     setHotels(hotelsData);
-  //   } catch (error) {
-  //     console.error("Error fetching hotels:", error);
-  //   }
-  // };
+   const getUsers = async () => {
+      try {
+         const empCollectionRef = collection(db, "places");
+         const data = await getDocs(empCollectionRef);
+         setRows(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+         console.log("fetched hotels:", data.docs);
+      } catch (error) {
+         console.error("Error fetching hotels:", error);
+      }
+   };
 
-  // useEffect(() => {
+   // const fetchHotels = async () => {
+   //   try {
+   //     const hotelsCollection = collection(db, "places");
+   //     const hotelsSnapshot = await getDocs(hotelsCollection);
+   //     const hotelsData = hotelsSnapshot.docs.map((doc) => ({
+   //       ...doc.data(),
+   //       id: doc.id
+   //     }));
+   //     console.error("fetched hotels:", hotelsData);
+   //     setHotels(hotelsData);
+   //   } catch (error) {
+   //     console.error("Error fetching hotels:", error);
+   //   }
+   // };
 
-  //   fetchHotels();
-  // }, []); // Run only once on component mount
-  
+   // useEffect(() => {
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+   //   fetchHotels();
+   // }, []); // Run only once on component mount
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+   const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+   };
 
-  return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+   const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+   };
+
+   return (
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
          <Typography
             gutterBottom
             variant="h5"
             component="div"
             sx={{ padding: "20px" }}
-          >
+         >
             Products List
-          </Typography>
-          <Divider />
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-                <TableCell
-                  align="left"
-                  style={{ minWidth: "100px" }}
-                >
-                    Name
-                  
-                </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1}>
-                        <TableCell key={row.id} align="left">
-                      {row.name}
-                        </TableCell>
+         </Typography>
+         <Divider />
+         <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+               <TableHead>
+                  <TableRow>
+                     <TableCell align="left" style={{ minWidth: "100px" }}>
+                        Place
+                     </TableCell>
+                     <TableCell align="left" style={{ minWidth: "100px" }}>
+                        Picture
+                     </TableCell>
+                     <TableCell align="left" style={{ minWidth: "100px" }}>
+                        Name
+                     </TableCell>
+                     <TableCell align="left" style={{ minWidth: "100px" }}>
+                        Address
+                     </TableCell>
+                     
                   </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
+               </TableHead>
+               <TableBody>
+                  {rows
+                     .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                     )
+                     .map((row) => {
+                        return (
+                           <TableRow key={row.id} hover role="checkbox" tabIndex={-1}>
+                              <TableCell  align="left">
+                                 {row.place}
+                              </TableCell>
+                              <TableCell  align="left">
+                                 {/* {row.place} */}
+                                 <Link to={row.placeImage} target="_blank">
+                                    <img src={row.placeImage} width={80} height={35}/>
+                                 </Link>
+                              </TableCell>
+                              <TableCell  align="left">
+                                 {row['properties'][0]['name']}
+                              </TableCell>
+                              <TableCell  align="left">
+                                 {row['properties'][0]['address']}
+                              </TableCell>
+                           </TableRow>
+                        );
+                     })}
+               </TableBody>
+            </Table>
+         </TableContainer>
+         <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+         />
+      </Paper>
+   );
 }
